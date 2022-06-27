f74c272131fd84f0afa8e32edcba33e5490143b3
a <a@DESKTOP-1DE6U51>
2022-06-27 13:31:03 +0430

    layout category

---
+++ Samanik.Web/ViewComponents/ArticleCategoryViewComponent.cs
@@ -0,0 +1,27 @@
+﻿using Data.Contracts;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Samanik.Web.ViewComponents
+{
+
+    public class ArticleCategoryViewComponent :  ViewComponent
+    {
+        private readonly IArticelCategoryRepasitory _categoryRepository;
+
+        public ArticleCategoryViewComponent(IArticelCategoryRepasitory categoryRepository)
+        {
+            _categoryRepository = categoryRepository;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var categories = _categoryRepository.GetListArticleCategory();
+
+            return View(categories);
+        }
+    }
+}
